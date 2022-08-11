8eaba84ccfdd58a4e53a5872dbf2ea1904413c86
Theo Crandall <thrandale@gmail.com>
2022-08-11 00:23:47 -0600

    test

--- server/index.js
+++ server/index.js
@@ -2,22 +2,23 @@ const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 const PORT = process.env.PORT || 3000;
 
-// receive
-// server.on("message", (msg, rInfo) => messageHandler(msg, rInfo));
-// server.bind(PORT);
-// console.log(`Server running on port ${PORT}`)
+//receive
+console.log("starting");
+server.on("message", (msg, rInfo) => messageHandler(msg, rInfo));
+server.bind(PORT);
+console.log(`Server running on port ${PORT}`)
 
-// let messageHandler = function (msg, rInfo) {
-//     console.log("Message Received: " + msg.toString());
+let messageHandler = function (msg, rInfo) {
+    console.log("Message Received: " + msg.toString());
 
-//     var responseString = "message from server (HI)";
+    var responseString = "message from server (HI)";
 
-//     // send
-//     server.send(responseString, 0, responseString.length, rInfo.port, rInfo.address,
-//         function (err, sent) {
-//             if (err)
-//                 console.log("Error sending response");
-//             else
-//                 console.log("Responded to client at " + rInfo.address + ":" + rInfo.port);
-//         });
-// }
+    // send
+    server.send(responseString, 0, responseString.length, rInfo.port, rInfo.address,
+        function (err, sent) {
+            if (err)
+                console.log("Error sending response");
+            else
+                console.log("Responded to client at " + rInfo.address + ":" + rInfo.port);
+        });
+}
